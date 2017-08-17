3fa70f8d5d8c6e1af2a45b6425c87f6363b66a66
Jared <Jared.o@live.com>
2017-08-17 21:28:34 +1200

    [Desktop] Changes.

---
+++ SDVUnitTestProject1TicketPricingPart2/SDVUnitTestProject1TicketPricingPart2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SDVUnitTestProject1TicketPricingPart2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SDVUnitTestProject1TicketPricingPart2")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7f2796cd-f7b1-4796-a413-0afef13a2d67")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SDVUnitTestProject1TicketPricingPart2/SDVUnitTestProject1TicketPricingPart2/UnitTest1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FastwayCourier;
+
+namespace SDVUnitTestProject1TicketPricing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetTicketPriceOfNelson()
+        {
+            //arrange
+            ParcelQuoteFromNelson quoteFromNelson = new ParcelQuoteFromNelson();
+
+            //act
+            ParcelQuoteResult quoteResult = quoteFromNelson.CalculateQuote(12, "Pink");
+
+            //assert
+            Assert.AreEqual(0, quoteResult.ExcessTickets);
+            Assert.AreEqual(4.15m, quoteResult.Price);
+        }
+
+        [TestMethod]
+        public void GetTicketPriceOfHavelock()
+        {
+            //arrange
+            ParcelQuoteFromNelson quoteFromHavelock = new ParcelQuoteFromNelson();
+
+            //act
+            ParcelQuoteResult quoteResult = quoteFromHavelock.CalculateQuote(12, "Blue");
+
+            //assert
+            Assert.AreEqual(0, quoteResult.ExcessTickets);
+            Assert.AreEqual(6.95m, quoteResult.Price);
+        }
+
+        [TestMethod]
+        public void GetTicketPriceOfMurchison()
+        {
+            //arrange
+            ParcelQuoteFromNelson quoteFromMurchison = new ParcelQuoteFromNelson();
+
+            //act
+            ParcelQuoteResult quoteResult = quoteFromMurchison.CalculateQuote(12, "Lime");
+
+            //assert
+            Assert.AreEqual(0, quoteResult.ExcessTickets);
+            Assert.AreEqual(8.70m, quoteResult.Price);
+        }
+
+        [TestMethod]
+        public void GetTicketPriceOfReefton()
+        {
+            //arrange
+            ParcelQuoteFromNelson quoteFromReefton = new ParcelQuoteFromNelson();
+
+            //act
+            ParcelQuoteResult quoteResult = quoteFromReefton.CalculateQuote(12, "Orange");
+
+            //assert
+            Assert.AreEqual(0, quoteResult.ExcessTickets);
+            Assert.AreEqual(12.95m, quoteResult.Price);
+        }
+
+        [TestMethod]
+        public void GetTicketPriceOfReeftonOno()
+        {
+            //arrange
+            ParcelQuoteFromNelson quoteFromReefton = new ParcelQuoteFromNelson();
+
+            //act
+            ParcelQuoteResult quoteResult = quoteFromReefton.CalculateQuote(16, "Orange");
+
+            //assert
+            Assert.AreEqual(1, quoteResult.ExcessTickets);
+            Assert.AreEqual(19.15m, quoteResult.Price);
+        }
+
+        [TestMethod]
+        public void GetTicketPriceOfReeftonDos()
+        {
+            //arrange
+            ParcelQuoteFromNelson quoteFromReefton = new ParcelQuoteFromNelson();
+
+            //act
+            ParcelQuoteResult quoteResult = quoteFromReefton.CalculateQuote(22, "Orange");
+
+            //assert
+            Assert.AreEqual(2, quoteResult.ExcessTickets);
+            Assert.AreEqual(25.35m, quoteResult.Price);
+        }
+
+        [TestMethod]
+        public void GetTicketPriceOfMurchisonOno()
+        {
+            //arrange
+            ParcelQuoteFromNelson quoteFromMurchison = new ParcelQuoteFromNelson();
+
+            //act
+            ParcelQuoteResult quoteResult = quoteFromMurchison.CalculateQuote(16, "Lime");
+
+            //assert
+            Assert.AreEqual(1, quoteResult.ExcessTickets);
+            Assert.AreEqual(14.90m, quoteResult.Price);
+        }
+
+        [TestMethod]
+        public void GetTicketPriceOfMurchisonDos()
+        {
+            //arrange
+            ParcelQuoteFromNelson quoteFromMurchison = new ParcelQuoteFromNelson();
+
+            //act
+            ParcelQuoteResult quoteResult = quoteFromMurchison.CalculateQuote(16, "Lime");
+
+            //assert
+            Assert.AreEqual(1, quoteResult.ExcessTickets);
+            Assert.AreEqual(14.90m, quoteResult.Price);
+        }
+
+    }
+}
